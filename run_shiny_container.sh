94f2663546897604f034ae2eb31eaa2e157f9723
Alec Luro <alec.b.luro@gmail.com>
2021-02-01 13:21:53 -0600

    change port for aws

--- run_shiny_container.sh
+++ run_shiny_container.sh
@@ -11,6 +11,6 @@ echo "USGS-BBS dashboard image ready!"
 # Run Temporary Docker Container ----
 echo "Starting temporary docker container."
 
-docker run --rm -p 3838:3838 --name usgs-bbs-dashboard-container-shiny  usgs-bbs-dashboard
+docker run --rm -p 80:3838 --name usgs-bbs-dashboard-container-shiny  usgs-bbs-dashboard
 
 echo "Container ready! Go to http://localhost:3838 (on Linux OS) or http://<your ip address here>:3838 (Windows and Mac OS) to enter Shiny session"
